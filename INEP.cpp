37db52c67fc52ac41d64a32c4c714864b3752b34
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2024-09-22 12:19:51 +0200

    mod.

--- INEP.cpp
+++ INEP.cpp
@@ -12,8 +12,33 @@ void menuPrincipal() {
     std::cout << "4. Sortir \n";
     std::cout << "Escriu un nombre:";
 }
+//submenus
+void subMenuPrincipal1() {
 
+    std::cout << "1. Registre usuari \n";
+    std::cout << "2. Consulta usuari \n";
+    std::cout << "3. Modifica usuari \n";
+    std::cout << "4. Esborra usuari \n";
+    std::cout << "5. Tornar \n";
+    std::cout << "Escriu un nombre:";
+}
+void subMenuPrincipal2() {
 
+    std::cout << "1. Gestio pel.licules \n";
+    std::cout << "2. Gestio series \n";
+    std::cout << "3. Tornar \n";
+    std::cout << "Escriu un nombre:";
+}
+void subMenuPrincipal3() {
+
+    std::cout << "1. Consulta per qualificacio de edat \n";
+    std::cout << "2. Ultimes novetats \n";
+    std::cout << "3. Proximes estrenes \n";
+    std::cout << "4. Tornar \n";
+    std::cout << "Escriu un nombre:";
+}
+
+//subfuncions1
 void procesarRegistreUsuari() {
 
     std::string nom;
@@ -34,26 +59,83 @@ void procesarRegistreUsuari() {
 }
 void procesarConsultaUsuari() {
 
+    std::string nom;
+    std::string cognom;
+    std::string sobrenom;
+    std::cout << "Escriure nom: ";
+    std::cin >> nom;
+    std::cout << "Escriure congnom: ";
+    std::cin >> cognom;
+    std::cout << "Escriure sobrenom: ";
+    std::cin >> sobrenom;
+    if (nom == " " or cognom == " " or sobrenom == " ") {
+        std::cout << "Aquest usuari no esta registrat \n";
+    }
+    else {
+        std::cout << "El usuari " + nom + " " + cognom + " ( " + sobrenom + " ) esta registrat. \n";
+    }
 }
 void procesarModificaUsuari() {
 
+    std::string nom;
+    std::string cognom;
+    std::string sobrenom;
+    std::string apartat;
+    std::cout << "Escriure nom: ";
+    std::cin >> nom;
+    std::cout << "Escriure congnom: ";
+    std::cin >> cognom;
+    std::cout << "Escriure sobrenom: ";
+    std::cin >> sobrenom;
+    std::cout << "Quin dels apartats anterior vols modificar 1,2 o 3 ?";
+    std::cin >> apartat;
+    if (apartat == "1") {
+        
+        std::cout << "Escriure nom: ";
+        std::cin >> nom;
+        std::cout << "El usuari " + nom + " " + cognom + " ( " + sobrenom + " ) s,ha modificat correctament. \n";
+    }
+    else if (apartat == "2") {
+
+        std::cout << "Escriure cognom: ";
+        std::cin >> cognom;
+        std::cout << "El usuari " + nom + " " + cognom + " ( " + sobrenom + " ) s,ha modificat correctament. \n";
+    }
+    else if (apartat == "3") {
+
+        std::cout << "Escriure sobrenom: ";
+        std::cin >> sobrenom;
+        std::cout << "El usuari " + nom + " " + cognom + " ( " + sobrenom + " ) s,ha modificat correctament. \n";
+    }
+    else {
+
+        std::cout << "Apartat no trobat. \n";
+    }
 }
 void procesarEsborraUsuari() {
-
+ 
+    std::string nom;
+    std::string cognom;
+    std::string sobrenom;
+    std::cout << "Escriure nom: ";
+    std::cin >> nom;
+    std::cout << "Escriure congnom: ";
+    std::cin >> cognom;
+    std::cout << "Escriure sobrenom: ";
+    std::cin >> sobrenom;
+    if (nom == " " or cognom == " " or sobrenom == " ") {
+        std::cout << "Error al esborrar usuari \n";
+    }
+    else {
+        std::cout << "El usuari " + nom + " " + cognom + " ( " + sobrenom + " ) s,ha esborrat correctament. \n";
+    }
 }
 
 //principal 1
 void gestioUsuari() {
 
-    std::cout << "1. Registre usuari \n";
-    std::cout << "2. Consulta usuari \n";
-    std::cout << "3. Modifica usuari \n";
-    std::cout << "4. Esborra usuari \n"; 
-    std::cout << "5. Tornar \n";
-    
     std::string entrada2;
-
-    std::cout << "Escriu un nombre:";
+    subMenuPrincipal1();
     bool continua = true;
 
     while (continua and std::cin >> entrada2) {
@@ -76,27 +158,43 @@ void gestioUsuari() {
             std::cout << "Has escollit l'opcio " + entrada2 + ". Tornar \n";
             continua = false;
         }
+        if (continua) {
+            subMenuPrincipal1();
+        }
     }
 }
 
+//subfuncions2
 void procesarGestioPelicules(){
+
+    std::string nom_peli;
+    std::cout << "Escriu el nom de la pel.licula que busques: ";
+    std::cin >> nom_peli;
+    if (nom_peli == " ") {
+        std::cout << "No s,ha trobat la pel.licula que busques. \n";
+    }
+    else {
+        std::cout << "La pel.licula " + nom_peli + " s,ha trobat. \n";
+    }
 }
 void procesarGestioSeries(){
+
+    std::string nom_serie;
+    std::cout << "Escriu el nom de la serie que busques: ";
+    std::cin >> nom_serie;
+    if (nom_serie == " ") {
+        std::cout << "No s,ha trobat la serie que busques. \n";
+    }
+    else {
+        std::cout << "La serie " + nom_serie + " s,ha trobat. \n";
+    }
 }
 
 //principal 2
 void gestioContinguts() {
 
-    std::cout << "1. Gestio pel.licules \n";
-    std::cout << "2. Gestio series \n";
-    std::cout << "3. Tornar \n";
-
     std::string entrada2;
-
-    std::cout << "Escriu un nombre:";
-    std::cin >> entrada2;
-    std::cout << "Has escollit el nombre " + entrada2 + "\n";
-
+    subMenuPrincipal2();
     bool continua = true;
 
     while (continua and std::cin >> entrada2) {
@@ -116,26 +214,45 @@ void gestioContinguts() {
             continua = false;
             //procesarTornar();
         }
+        if (continua) {
+            subMenuPrincipal2();
+        }
     }
 }
 
-void procesarConsultaPerQualificacioDeEdat(){}
-void procesarUltimesNovetats(){}
-void procesarProximesEstrenes(){}
+//subfuncions3
+void procesarConsultaPerQualificacioDeEdat(){
+       
+    int edat;
+    std::cout << "Escriure la teva edat actual: ";
+    std::cin >> edat;
+
+    if (edat > 0 and edat <= 12) {
+        std::cout << "Mostrant apartat infantil. \n \n";
+    }
+    else if (edat > 12 and edat < 18) {
+    
+        std::cout << "Mostrant apartat juvenil. \n \n";
+    }
+    else {
+
+        std::cout << "Mostrant apartat adult. \n \n";
+    }
+}
+void procesarUltimesNovetats(){
+
+    std::cout << "Mostrant les ultimes novetats. \n\n";
+}
+void procesarProximesEstrenes(){
+
+    std::cout << "Mostrant les proximes estrenes. \n\n";
+}
 
 //pricipal 3
 void consultes() {
 
-    std::cout << "1. Consulta per qualificacio de edat \n";
-    std::cout << "2. Ultimes novetats \n";
-    std::cout << "3. Proximes estrenes \n";
-    std::cout << "4. Tornar \n";
-
     std::string entrada2;
-
-    std::cout << "Escriu un nombre:";
-    std::cin >> entrada2;
-
+    subMenuPrincipal3();
     bool continua = true;
 
     while (continua and std::cin >> entrada2) {
@@ -157,15 +274,19 @@ void consultes() {
             std::cout << "Has escollit l'opcio " + entrada2 + ". Tornar \n";
             continua = false;
         }
+        if (continua) {
+            subMenuPrincipal3();
+        }
     }
 }
 
 int main()
 {
-    std::string entrada;
     menuPrincipal();
+    bool continua_programa = true;
+    std::string entrada;
 
-    while (std::cin >> entrada){
+    while (continua_programa and std::cin >> entrada){
         if (entrada == "1") {
 
             std::cout << "Has escollit l'opcio " + entrada + ". Gestio usuaris \n";
@@ -184,9 +305,12 @@ int main()
         else {
 
             std::cout << "Has escollit l'opcio " + entrada + ". Sortir \n";
+            continua_programa = false;
             // bool para sortir 
         }
-        menuPrincipal();
+        if (continua_programa) {
+            menuPrincipal();
+        }
     }
 }
 
