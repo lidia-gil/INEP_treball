128cbb26262eee62e2c610d2f713f33bd602951e
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2024-09-22 09:00:10 +0200

    ult.

--- INEP.cpp
+++ INEP.cpp
@@ -3,10 +3,50 @@
 
 #include <iostream>
 
+//menu principal
+void menuPrincipal() {
+
+    std::cout << "1. Gestio usuaris \n";
+    std::cout << "2. Gestio continguts \n";
+    std::cout << "3. Consultes \n";
+    std::cout << "4. Sortir \n";
+    std::cout << "Escriu un nombre:";
+}
+
+
 void procesarRegistreUsuari() {
 
+    std::string nom;
+    std::string cognom;
+    std::string sobrenom;
+    std::cout << "Escriure nom: ";
+    std::cin >> nom;
+    std::cout << "Escriure congnom: ";
+    std::cin >> cognom;
+    std::cout << "Escriure sobrenom: ";
+    std::cin >> sobrenom;
+    if (nom == " " or cognom == " " or sobrenom == " ") {
+        std::cout << "Error al registrar usuari \n";
+    }
+    else {
+        std::cout << "El registre de el usuari " + nom + " " + cognom + " ( " + sobrenom + " ) s,ha processat correctament. \n";
+    }
+}
+void procesarConsultaUsuari() {
+
+}
+void procesarModificaUsuari() {
+
+}
+void procesarEsborraUsuari() {
+
+}
+
+//principal 1
+void gestioUsuari() {
+
     std::cout << "1. Registre usuari \n";
-    std::cout << "2. Gestio continguts \n";
+    std::cout << "2. Consulta usuari \n";
     std::cout << "3. Modifica usuari \n";
     std::cout << "4. Esborra usuari \n"; 
     std::cout << "5. Tornar \n";
@@ -14,30 +54,40 @@ void procesarRegistreUsuari() {
     std::string entrada2;
 
     std::cout << "Escriu un nombre:";
-    std::cin >> entrada2;
-    std::cout << "Has escollit el nombre " + entrada2 + "\n";
+    bool continua = true;
 
-    if (entrada2 == "1") {
+    while (continua and std::cin >> entrada2) {
 
-    }
-    else if (entrada2 == "2") {
+        if (entrada2 == "1") {
 
-    }
-    else if (entrada2 == "3") {
-
-    }
-    else if (entrada2 == "4") {
-
-    }
-    else {
-
-        //ha de tornar, com??? ns 
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Registre usuari \n";
+            procesarRegistreUsuari();
+        }
+        else if (entrada2 == "2") {
+            procesarConsultaUsuari();
+        }
+        else if (entrada2 == "3") {
+            procesarModificaUsuari();
+        }
+        else if (entrada2 == "4") {
+            procesarEsborraUsuari();
+        }
+        else {
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Tornar \n";
+            continua = false;
+        }
     }
 }
 
-void procesarGestioContinguts() {
+void procesarGestioPelicules(){
+}
+void procesarGestioSeries(){
+}
 
-    std::cout << "1. Gestio pelicules \n";
+//principal 2
+void gestioContinguts() {
+
+    std::cout << "1. Gestio pel.licules \n";
     std::cout << "2. Gestio series \n";
     std::cout << "3. Tornar \n";
 
@@ -47,85 +97,96 @@ void procesarGestioContinguts() {
     std::cin >> entrada2;
     std::cout << "Has escollit el nombre " + entrada2 + "\n";
 
-    if (entrada2 == "1") {
+    bool continua = true;
 
+    while (continua and std::cin >> entrada2) {
+        if (entrada2 == "1") {
+
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Gestio pel.licules \n";
+            procesarGestioPelicules();
+        }
+        else if (entrada2 == "2") {
+
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Gestio series \n";
+            procesarGestioSeries();
+        }
+        else {
+
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Tornar \n";
+            continua = false;
+            //procesarTornar();
+        }
     }
-    else if (entrada2 == "2") {
-
-    }
-    else {
-
-    }
-
 }
 
-void procesarConsultes() {
+void procesarConsultaPerQualificacioDeEdat(){}
+void procesarUltimesNovetats(){}
+void procesarProximesEstrenes(){}
 
-    std::cout << "1. Consulta per qualificació d’edat \n";
-    std::cout << "2. Últimes novetats \n";
-    std::cout << "3. Pròximes estrenes \n";
+//pricipal 3
+void consultes() {
+
+    std::cout << "1. Consulta per qualificacio de edat \n";
+    std::cout << "2. Ultimes novetats \n";
+    std::cout << "3. Proximes estrenes \n";
     std::cout << "4. Tornar \n";
 
     std::string entrada2;
 
     std::cout << "Escriu un nombre:";
     std::cin >> entrada2;
-    std::cout << "Has escollit el nombre " + entrada2 + "\n";
 
-    if (entrada2 == "1") {
+    bool continua = true;
 
-    }
-    else if (entrada2 == "2") {
+    while (continua and std::cin >> entrada2) {
 
-    }
-    else if (entrada2 == "3") {
+        if (entrada2 == "1") {
 
-    }
-    else {
+            procesarConsultaPerQualificacioDeEdat();
+        }
+        else if (entrada2 == "2") {
 
+            procesarUltimesNovetats();
+        }
+        else if (entrada2 == "3") {
+
+            procesarProximesEstrenes();
+        }
+        else {
+
+            std::cout << "Has escollit l'opcio " + entrada2 + ". Tornar \n";
+            continua = false;
+        }
     }
 }
 
 int main()
 {
-    std::cout << "1. Gestio usuaris \n";
-    std::cout << "2. Gestio continguts \n";
-    std::cout << "3. Consultes \n";
-    std::cout << "4. Sortir \n";
-
     std::string entrada;
+    menuPrincipal();
 
-    std::cout << "Escriu un nombre:";
-    std::cin >> entrada;
-    std::cout << "Has escollit el nombre " + entrada + "\n";
+    while (std::cin >> entrada){
+        if (entrada == "1") {
 
-    if (entrada == "1") {
+            std::cout << "Has escollit l'opcio " + entrada + ". Gestio usuaris \n";
+            gestioUsuari();
+        }
+        else if (entrada == "2") {
 
-        std::cout << "Has escollit l'opcio Gestio usuaris \n";
-        procesarRegistreUsuari();
-    }
-    else if (entrada == "2") {
+            std::cout << "Has escollit l'opcio " + entrada + ". Gestio continguts \n";
+            gestioContinguts();
+        }
+        else if (entrada == "3") {
 
-        std::cout << "Has escollit l'opcio Gestio continguts \n";
-        procesarGestioContinguts();
-    }
-    else if (entrada == "3") {
+            std::cout << "Has escollit l'opcio " + entrada + ". Consultes \n";
+            consultes();
+        }
+        else {
 
-        std::cout << "Has escollit l'opcio Consultes \n";
-        void procesarConsultes();
-    }
-    else {
-
-        std::cout << "Has escollit l'opcio Sortir \n";
+            std::cout << "Has escollit l'opcio " + entrada + ". Sortir \n";
+            // bool para sortir 
+        }
+        menuPrincipal();
     }
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
