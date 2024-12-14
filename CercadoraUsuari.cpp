4fa795daa0f3081baae436e95743f3399c9eb018
Pau Campillos <paucampillos@gmail.com>
2024-12-15 00:11:29 +0100

    .

--- CercadoraUsuari.cpp
+++ CercadoraUsuari.cpp
@@ -13,12 +13,10 @@ PassarelaUsuari CercadoraUsuari::cercaPerSobrenom(std::string sobrenomU) {
     sql::ResultSet* res = con.executarConsulta(sql);
     // Si no troba cap fila, activa excepció
     if (!res->next()) {
-        std::cout << "ERROR" << std::endl;
         //throw runtime_error("Usuari no existeix");
 
     }
     else {
-        std::cout << "CORRECTE" << std::endl;
         u.posaSobrenom(res->getString("sobrenom"));
         u.posaNom(res->getString("nom"));
         u.posaCorreu(res->getString("correu_electronic"));
