7040a6f606fef442162a69462aca0fb65b0300ee
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2024-12-22 10:22:52 +0100

    Modificat per Pau i Lidia ( avanç cas d'Us consulta Visu )

---
+++ CercadoraContingut.cpp
@@ -0,0 +1,23 @@
+#include "CercadoraContingut.h"
+
+
+PassarelaContingut cercaPerTitol(std::string titolC) {
+
+    PassarelaContingut contingut;
+    ConnexioBD con;
+    std::string sql = "SELECT * FROM contingut WHERE titol = '" + titolC + "'";
+    sql::ResultSet* res = con.executarConsulta(sql);
+    // Si no troba cap fila, activa excepciÃ³
+    if (!res->next()) {
+        //throw exception("Usuari no existeix");  // no ens funciona aquesta operació 
+        throw std::runtime_error("El contingut consultat no existeix");
+    }
+    else {
+        contingut.posaTitol(res->getString("titol"));
+        contingut.posaDescripcio(res->getString("descripcio"));
+        contingut.posaQualificacioEdat(res->getString("qualificacio"));
+        contingut.posaTipus(res->getString("tipus"));
+        delete res;
+    }
+    return contingut;
+}
